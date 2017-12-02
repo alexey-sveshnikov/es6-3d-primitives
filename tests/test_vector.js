95d8d2945951468b50663ca36ac56f46514601d6
Alexey Sveshnikov <alexey.sveshnikov@gmail.com>
2017-12-03 00:13:34 +0300

    Initial version (only Vectors are implemented at this point)

---
+++ tests/test_vector.js
@@ -0,0 +1,131 @@
+'use strict';
+
+import {Vector, VectorError} from '../vector.js';
+import {test} from 'ava';
+
+function check_vector_equals(t, v1, v2) {
+    t.true(v1.equals(v2));
+}
+
+test('Test vector comparing', t => {
+    let v = new Vector(1, 2, 3);
+
+    check_vector_equals(t, v, v);
+    check_vector_equals(t, v, new Vector(1, 2, 3));
+
+    t.false(v.equals(new Vector(0, 2, 3)));
+    t.false(v.equals(new Vector(1, 0, 3)));
+    t.false(v.equals(new Vector(1, 2, 0)));
+});
+
+test('Test vector addition', t => {
+    const v1 = new Vector(1, 2, 3);
+    const v2 = new Vector(4, 5, 6);
+
+    const result = v1.add(v2);
+
+    check_vector_equals(t, result, new Vector(5, 7, 9));
+
+    // Test that operands are unchanged
+    t.true(v1.equals(new Vector(1, 2, 3)));
+    t.true(v2.equals(new Vector(4, 5, 6)));
+});
+
+test('Test vector subtraction', t => {
+    const v1 = new Vector(1, 2, 3);
+    const v2 = new Vector(3, 2, 1);
+
+    const result = v1.sub(v2);
+
+    check_vector_equals(t, result, new Vector(-2, 0, 2));
+
+    // Test that operands are unchanged
+    t.true(v1.equals(new Vector(1, 2, 3)));
+    t.true(v2.equals(new Vector(3, 2, 1)));
+});
+
+test('Test vector multiplication (by scalar)', t => {
+    const v1 = new Vector(1, 2, 3);
+
+    const result = v1.mul(10);
+
+    check_vector_equals(t, result, new Vector(10, 20, 30));
+
+    // Test that operands are unchanged
+    t.true(v1.equals(new Vector(1, 2, 3)));
+});
+
+test('Test vector multiplication (by vector)', t => {
+    const v1 = new Vector(1, 2, 3);
+    const v2 = new Vector(5, 7, 11);
+
+    const result = v1.mul(v2);
+
+    check_vector_equals(t, result, new Vector(5, 14, 33));
+
+    // Test that operands are unchanged
+    t.true(v1.equals(new Vector(1, 2, 3)));
+    t.true(v2.equals(new Vector(5, 7, 11)));
+});
+
+test('Test vector cross product', t => {
+    const v1 = new Vector(1, 2, 3);
+    const v2 = new Vector(3, 2, 1);
+
+    const result = v1.cross(v2);
+
+    check_vector_equals(t, result, new Vector(-4, 8, -4));
+
+    t.true(v1.cross(v1).equals(new Vector(0, 0, 0)));
+
+    // Test that operands are unchanged
+//    t.true(v1.equals(new Vector(1, 2, 3)));
+ //   t.true(v2.equals(new Vector(3, 2, 1)));
+});
+
+test('Test vector multiplication (invalid operand)', t => {
+    const v1 = new Vector(1, 2, 3);
+
+    t.throws(() => {
+        v1.mul('3');
+    }, VectorError);
+});
+
+test('Test vectors dot product', t => {
+    const v1 = new Vector(1, 2, 3);
+    const v2 = new Vector(5, 7, 11);
+
+    const result = v1.dot(v2);
+
+    t.is(result, 5 + 14 + 33);
+
+    // Test that operands are unchanged
+    t.true(v1.equals(new Vector(1, 2, 3)));
+    t.true(v2.equals(new Vector(5, 7, 11)));
+});
+
+
+test('Test vectors negation operation', t => {
+    const v1 = new Vector(1, 2, 3);
+
+    check_vector_equals(t, v1.neg(), new Vector(-1, -2, -3));
+});
+
+test('Test vectors length calculation', t => {
+    t.is(new Vector(3,4,0).length(), 5);
+    t.is(new Vector(0,3,4).length(), 5);
+});
+
+test('Test vectors squared length calculation', t => {
+    t.is(new Vector(3,4,0).length2(), 25);
+    t.is(new Vector(0,3,4).length2(), 25);
+});
+
+test('Test vectors normalize operation', t => {
+    const v1 = new Vector(100, 200, 300);
+    const v2 = new Vector(1000, 2000, 3000);
+
+    t.is(v1.normalize().length(), 1);
+
+    check_vector_equals(t, v1.normalize(), v2.normalize())
+});
---
+++ vector.js
@@ -0,0 +1,76 @@
+'use strict';
+
+export class VectorError extends Error {}
+
+export class Vector {
+    constructor(x, y, z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    add(v) {
+        return new Vector(this.x + v.x, this.y + v.y, this.z + v.z);
+    }
+
+    sub(v) {
+        return new Vector(this.x - v.x, this.y - v.y, this.z - v.z);
+    }
+
+    mul(v) {
+        if (v instanceof Vector) {
+            return new Vector(this.x * v.x, this.y * v.y, this.z * v.z);
+        } else if (typeof(v) == 'number') {
+            return new Vector(this.x * v, this.y * v, this.z * v);
+        } else {
+            throw new VectorError('Vector.mul() operand must be a vector or scalar')
+        }
+    }
+
+    div(v) {
+        if (v instanceof Vector) {
+            return new Vector(this.x / v.x, this.y / v.y, this.z / v.z);
+        } else if (typeof(v) == 'number') {
+            return new Vector(this.x / v, this.y / v, this.z / v);
+        } else {
+            throw new VectorError('Vector.div() operand must be a vector or scalar')
+        }
+    }
+
+    cross(v) {
+        return new Vector(
+            this.y * v.z - this.z * v.y,
+            this.z * v.x - this.x * v.z,
+            this.x * v.y - this.y * v.x
+        )
+    }
+
+    neg() {
+        return new Vector(-this.x, -this.y, -this.z);
+    }
+
+    normalize() {
+        return this.div(this.length());
+    }
+
+
+    equals(v) {
+        return this.x == v.x && this.y == v.y && this.z == v.z;
+    }
+
+    dot(v) {
+        return this.x * v.x + this.y * v.y + this.z * v.z;
+    }
+
+    length() {
+        return Math.sqrt(this.dot(this))
+    }
+
+    length2() {
+        return this.dot(this)
+    }
+
+    toString() {
+        return `Vector(x: ${this.x}, y: ${this.y}, z: ${this.z})`
+    }
+}
